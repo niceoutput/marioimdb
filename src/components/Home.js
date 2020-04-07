258c5c3c231502f8b11092976242de4dcade4d7e
mario <mario@uniplaces.com>
2020-04-07 15:43:23 +0100

    add custom hook, grid elements and load more thumbs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
     API_URL,
     API_KEY,
     IMAGE_BASE_URL,
-    BACKDROP_SIZE
+    BACKDROP_SIZE,
+    POSTER_SIZE
 } from '../config';
 import HeroImage from './elements/HeroImage';
 import SearchBar from './elements/SearchBar';
@@ -11,29 +12,61 @@ import Grid from './elements/Grid';
 import MovieThumb from './elements/MovieThumb';
 import LoadMoreBtn from './elements/LoadMoreBtn';
 import Spinner from './elements/Spinner';
+import NoImage from './images/no_image.jpg';
 
 //Custom hook
 import { useHomeFetch } from './hooks/useHomeFetch';
 
 const Home = () => {
-    const [{ state, loading, error }, fetchMovies] = useHomeFetch();
-    console.log(state);
+    const [
+        {
+            state: { movies, currentPage, totalPages, heroImage },
+            loading,
+            error
+        },
+        fetchMovies
+    ] = useHomeFetch();
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const loadMoreMovies = () => {
+        const searchEndpoint = `${API_URL}search/movie?api_key=${API_KEY}&query=${searchTerm}&page=${currentPage + 1}`;
+        const popularEndpoint = `${API_URL}/movie/popular?api_key=${API_KEY}&page=${currentPage + 1}`;
+
+        const endpoint = searchTerm ? searchEndpoint : popularEndpoint;
+
+        fetchMovies(endpoint);
+    }
 
     if (error) return <span>Something went wrong</span>
-    if (!state.movies[0]) return <Spinner />
+    if (!movies[0]) return <Spinner />
 
     return (
         <React.Fragment>
             <HeroImage
-                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
-                title={state.heroImage.original_title}
-                text={state.heroImage.overview}
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+                title={heroImage.original_title}
+                text={heroImage.overview}
             />
             <SearchBar />
-            <Grid />
-            <MovieThumb />
-            <Spinner />
-            <LoadMoreBtn />
+            <Grid header={searchTerm ? 'Search Result' : 'Popular movies'}>
+                {movies.map(movie => (
+                    <MovieThumb
+                        key={movie.id}
+                        clickable
+                        image={
+                            movie.poster_path
+                                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                                : NoImage
+                        }
+                        movieId={movie.id}
+                        movieName={movie.original_title}
+                    />
+                ))}
+            </Grid>
+            {loading && <Spinner />}
+            {currentPage < totalPages && !loading &&(
+                <LoadMoreBtn text="Load More" callback={loadMoreMovies} />
+            )}
         </React.Fragment>
     )
 }
--- src/components/elements/LoadMoreBtn.js
+++ src/components/elements/LoadMoreBtn.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { StyledLoadMoreBtn } from '../styles/StyledLoadMoreBtn';
 
-const LoadMoreBtn = () => (
-    <div>
-        Load More Button
-    </div>
+
+const LoadMoreBtn = ({ text, callback }) => (
+    <StyledLoadMoreBtn type="button" onClick={callback}>
+        {text}
+    </StyledLoadMoreBtn>
 )
 
 export default LoadMoreBtn;
