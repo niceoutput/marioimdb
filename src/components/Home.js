acc733780a03ad328144bc92ef87ab14bc7872ad
mario <mario@uniplaces.com>
2020-04-13 11:26:04 +0100

    add local storage and session storage

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,6 +18,7 @@ import NoImage from './images/no_image.jpg';
 import { useHomeFetch } from './hooks/useHomeFetch';
 
 const Home = () => {
+    const [searchTerm, setSearchTerm] = useState('');
     const [
         {
             state: { movies, currentPage, totalPages, heroImage },
@@ -25,8 +26,7 @@ const Home = () => {
             error
         },
         fetchMovies
-    ] = useHomeFetch();
-    const [searchTerm, setSearchTerm] = useState('');
+    ] = useHomeFetch(searchTerm);
 
     const searchMovies = search => {
         const endpoint = search ? `${SEARCH_BASE_URL}${search}` : `${POPULAR_BASE_URL}`;
